4b8f7da645d9cda64ec797fdda73b93daab87351
Lucas Levy <llukas1994@wp.pl>
2014-06-10 21:26:21 +0200

    Setup Enviroment
    Gradle, Eclipse, Java and beggining mod code

---
+++ src/main/java/pl/grm/narutocraftmod/items/ShunshinNoJutsu.java
@@ -0,0 +1,15 @@
+package pl.grm.narutocraftmod.items;
+
+import pl.grm.narutocraftmod.NarutoCraftMod;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+public class ShunshinNoJutsu extends Item{
+	
+	public ShunshinNoJutsu(int i) {
+		this.setUnlocalizedName("SNoJutsu");
+		this.setTextureName("narutocraftmod:NoJutsu");
+		this.setCreativeTab(NarutoCraftMod.mTabNarutoCraftMod);
+	}
+	
+}
---
+++ src/main/java/pl/grm/narutocraftmod/items/WKunai.java
@@ -0,0 +1,12 @@
+package pl.grm.narutocraftmod.items;
+
+import pl.grm.narutocraftmod.NarutoCraftMod;
+import net.minecraft.item.Item;
+
+public class WKunai extends Item {
+	public WKunai(int i) {
+		this.setUnlocalizedName("WybuchowyKunai");
+		this.setTextureName("narutocraftmod:Kunai");
+		this.setCreativeTab(NarutoCraftMod.mTabNarutoCraftMod);
+	}
+}
