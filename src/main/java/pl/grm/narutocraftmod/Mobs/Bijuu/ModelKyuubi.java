40acde26af9193e129e04a118937837a87fde4c4
Admaster <mizyn24@gmail.com>
2014-08-21 18:03:30 +0200

    Delete unnecesary Object3D class

--- src/main/java/pl/grm/narutocraftmod/Mobs/Bijuu/ModelKyuubi.java
+++ src/main/java/pl/grm/narutocraftmod/Mobs/Bijuu/ModelKyuubi.java
@@ -375,7 +375,7 @@ public class ModelKyuubi extends ModelBase { //ModelQuadruped {
 	    model.rotateAngleY = y;
 	    model.rotateAngleZ = z;
 	  }
-	  @O
+	  
 	  public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5, float f6, float f7, Entity entity)
 	  {
 	    super.setRotationAngles(f, f1, f2, f3, f4, f5, entity);
