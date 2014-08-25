d3c9e822869326db9c0f4437af46f3c8313d7aab
Admaster <mizyn24@gmail.com>
2014-08-25 21:00:11 +0200

    add sample

--- src/main/java/pl/grm/narutocraftmod/HUD/ContainerNCPLPlayer.java
+++ src/main/java/pl/grm/narutocraftmod/HUD/ContainerNCPLPlayer.java
@@ -9,134 +9,124 @@ import net.minecraft.item.ItemStack;
 
 import pl.grm.narutocraftmod.jutsu.Sharingan;
 
-public class ContainerNCPLPlayer extends Container{
-	/** Avoid magic numbers! This will greatly reduce the chance of you making errors in 'transferStackInSlot' method */
-	private static final int ARMOR_START = InvPlayerSlot.INV_SIZE, ARMOR_END = ARMOR_START+3,
-	INV_START = ARMOR_END+1, INV_END = INV_START+26, HOTBAR_START = INV_END+1,
-	HOTBAR_END = HOTBAR_START+8;
-
-	public ContainerNCPLPlayer(EntityPlayer player, InventoryPlayer inventoryPlayer, InvPlayerSlot inventoryCustom)
-	{
-	int i;
-
-	// Add CUSTOM slots - we'll just add two for now, both of the same type.
-	// Make a new Slot class for each different item type you want to add
-	this.addSlotToContainer(new SlotCustom(inventoryCustom, 0, 80, 8));
-	this.addSlotToContainer(new SlotCustom(inventoryCustom, 1, 80, 26));
-
-	// Add ARMOR slots; note you need to make a public version of SlotArmor
-	// just copy and paste the vanilla code into a new class and change what you need
-	/*for (i = 0; i < 4; ++i)
-	{
-	this.addSlotToContainer(new SlotArmor(player, inventoryPlayer, inventoryPlayer.getSizeInventory() - 1 - i, 8, 8 + i * 18, i));
-	}*/
-
-	// Add vanilla PLAYER INVENTORY - just copied/pasted from vanilla classes
-	for (i = 0; i < 3; ++i)
-	{
-	for (int j = 0; j < 9; ++j)
-	{
-	this.addSlotToContainer(new Slot(inventoryPlayer, j + i * 9 + 9, 8 + j * 18, 84 + i * 18));
-	}
-	}
-
-	// Add ACTION BAR - just copied/pasted from vanilla classes
-	for (i = 0; i < 9; ++i)
-	{
-	this.addSlotToContainer(new Slot(inventoryPlayer, i, 8 + i * 18, 142));
-	}
-	}
-
+public class ContainerNCPLPlayer extends Container {
 	/**
-	* This should always return true, since custom inventory can be accessed from anywhere
-	*/
+	 * Avoid magic numbers! This will greatly reduce the chance of you making
+	 * errors in 'transferStackInSlot' method
+	 */
+	private static final int ARMOR_START = InvPlayerSlot.INV_SIZE,
+			ARMOR_END = ARMOR_START + 3, INV_START = ARMOR_END + 1,
+			INV_END = INV_START + 26, HOTBAR_START = INV_END + 1,
+			HOTBAR_END = HOTBAR_START + 8;
+
+	public ContainerNCPLPlayer(EntityPlayer player,
+			InventoryPlayer inventoryPlayer, InvPlayerSlot inventoryCustom) {
+		int i;
+
+		/**
+		 * Adds CUSTOM slots new Slot class for each different item type
+		 */
+		this.addSlotToContainer(new SlotCustom(inventoryCustom, 0, 80, 8));
+		this.addSlotToContainer(new SlotCustom(inventoryCustom, 1, 80, 26));
+
+		/**
+		 * Add ARMOR slots; needs public version of SlotArmor copy and paste the
+		 * vanilla code into a new class and change what you need
+		 */
+		for (i = 0; i < 4; ++i) {
+			// this.addSlotToContainer(new SlotArmor(player, inventoryPlayer,
+			// inventoryPlayer.getSizeInventory() - 1 - i, 8, 8 + i * 18,
+			// i));
+		}
+
+		// Add PLAYER INVENTORY
+		for (i = 0; i < 3; ++i) {
+			for (int j = 0; j < 9; ++j) {
+				this.addSlotToContainer(new Slot(inventoryPlayer,
+						j + i * 9 + 9, 8 + j * 18, 84 + i * 18));
+			}
+		}
+
+		// Add ACTION BAR
+		for (i = 0; i < 9; ++i) {
+			this.addSlotToContainer(new Slot(inventoryPlayer, i, 8 + i * 18,
+					142));
+		}
+	}
+	/**
+	 * @return Always should be true
+	 */
 	@Override
-	public boolean canInteractWith(EntityPlayer player)
-	{
-	return true;
+	public boolean canInteractWith(EntityPlayer player) {
+		return true;
 	}
 
 	/**
-	* Called when a player shift-clicks on a slot. You must override this or you will crash when someone does that.
-	* Basically the same as every other container I make, since I define the same constant indices for all of them
-	*/
-	public ItemStack transferStackInSlot(EntityPlayer player, int par2)
-	{
-	ItemStack itemstack = null;
-	Slot slot = (Slot) this.inventorySlots.get(par2);
-
-	if (slot != null && slot.getHasStack())
-	{
-	ItemStack itemstack1 = slot.getStack();
-	itemstack = itemstack1.copy();
-
-	// Either armor slot or custom item slot was clicked
-	if (par2 < INV_START)
-	{
-	// try to place in player inventory / action bar
-	if (!this.mergeItemStack(itemstack1, INV_START, HOTBAR_END + 1, true))
-	{
-	return null;
-	}
-
-	slot.onSlotChange(itemstack1, itemstack);
-	}
-	// Item is in inventory / hotbar, try to place either in custom or armor slots
-	else
-	{
-	// if item is our custom item
-	if (itemstack1.getItem() instanceof Sharingan)
-	{
-	if (!this.mergeItemStack(itemstack1, 0, InvPlayerSlot.INV_SIZE, false))
-	{
-	return null;
-	}
-	}
-	// if item is armor
-	else if (itemstack1.getItem() instanceof ItemArmor)
-	{
-	int type = ((ItemArmor) itemstack1.getItem()).armorType;
-	if (!this.mergeItemStack(itemstack1, ARMOR_START + type, ARMOR_START + type + 1, false))
-	{
-	return null;
-	}
-	}
-	// item in player's inventory, but not in action bar
-	else if (par2 >= INV_START && par2 < HOTBAR_START)
-	{
-	// place in action bar
-	if (!this.mergeItemStack(itemstack1, HOTBAR_START, HOTBAR_START + 1, false))
-	{
-	return null;
-	}
-	}
-	// item in action bar - place in player inventory
-	else if (par2 >= HOTBAR_START && par2 < HOTBAR_END + 1)
-	{
-	if (!this.mergeItemStack(itemstack1, INV_START, INV_END + 1, false))
-	{
-	return null;
-	}
-	}
-	}
-
-	if (itemstack1.stackSize == 0)
-	{
-	slot.putStack((ItemStack) null);
-	}
-	else
-	{
-	slot.onSlotChanged();
-	}
-
-	if (itemstack1.stackSize == itemstack.stackSize)
-	{
-	return null;
-	}
-
-	slot.onPickupFromSlot(player, itemstack1);
-	}
-
-	return itemstack;
+	 * Called when a player shift-clicks on a slot. You must override this or
+	 * you will crash when someone does that. Basically the same as every other
+	 * container I make, since I define the same constant indices for all of
+	 * them
+	 */
+	@Override
+	public ItemStack transferStackInSlot(EntityPlayer player, int par2) {
+		ItemStack itemstack = null;
+		Slot slot = (Slot) this.inventorySlots.get(par2);
+		if (slot != null && slot.getHasStack()) {
+			ItemStack itemstack1 = slot.getStack();
+			itemstack = itemstack1.copy();
+			// Either armor slot or custom item slot was clicked
+			if (par2 < INV_START) {
+				// try to place in player inventory / action bar
+				if (!this.mergeItemStack(itemstack1, INV_START, HOTBAR_END + 1,
+						true)) {
+					return null;
+				}
+				slot.onSlotChange(itemstack1, itemstack);
+			}
+			// Item is in inventory / hotbar, try to place either in custom or
+			// armor slots
+			else {
+				// if item is our custom item
+				if (itemstack1.getItem() instanceof Sharingan) {
+					if (!this.mergeItemStack(itemstack1, 0,
+							InvPlayerSlot.INV_SIZE, false)) {
+						return null;
+					}
+				}
+				// if item is armor
+				else if (itemstack1.getItem() instanceof ItemArmor) {
+					int type = ((ItemArmor) itemstack1.getItem()).armorType;
+					if (!this.mergeItemStack(itemstack1, ARMOR_START + type,
+							ARMOR_START + type + 1, false)) {
+						return null;
+					}
+				}
+				// item in player's inventory, but not in action bar
+				else if (par2 >= INV_START && par2 < HOTBAR_START) {
+					// place in action bar
+					if (!this.mergeItemStack(itemstack1, HOTBAR_START,
+							HOTBAR_START + 1, false)) {
+						return null;
+					}
+				}
+				// item in action bar - place in player inventory
+				else if (par2 >= HOTBAR_START && par2 < HOTBAR_END + 1) {
+					if (!this.mergeItemStack(itemstack1, INV_START,
+							INV_END + 1, false)) {
+						return null;
+					}
+				}
+			}
+			if (itemstack1.stackSize == 0) {
+				slot.putStack((ItemStack) null);
+			} else {
+				slot.onSlotChanged();
+			}
+			if (itemstack1.stackSize == itemstack.stackSize) {
+				return null;
+			}
+			slot.onPickupFromSlot(player, itemstack1);
+		}
+		return itemstack;
 	}
 }
--- src/main/java/pl/grm/narutocraftmod/jutsu/UraRenge.java
+++ src/main/java/pl/grm/narutocraftmod/jutsu/UraRenge.java
@@ -1,12 +1,22 @@
 package pl.grm.narutocraftmod.jutsu;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 
 public class UraRenge extends Jutsu {
 
 	public UraRenge(int i) {
 		this.setUnlocalizedName("Ura Renge");
 		this.setTextureName("narutocraftmod:Ura Renge");
-		maxStackSize = 1;
+	}
+	@Override
+	public ItemStack onItemRightClick(ItemStack stack, World world,
+			EntityPlayer player) {
+		if (!world.isRemote) {
+
+		}
+		return stack;
 	}
 
 }
