79ff9b491aebc91a94e7dd6ba97186fdcdf9be56
MrFreesher <Artur@Artur-Komputer>
2014-08-25 21:21:30 +0200

    Add Jutsu Effect

--- src/main/java/pl/grm/narutocraftmod/effects/EffectsList.java
+++ src/main/java/pl/grm/narutocraftmod/effects/EffectsList.java
@@ -3,7 +3,7 @@ package pl.grm.narutocraftmod.effects;
  * Enum(List) of all effects
  */
 public enum EffectsList {
-	SLOW(0),STUN(1), jutsu(2);
+	jutsu(0),SLOWNESS(1),STUN(2),BARRIER(3),BLOCK(4),CONFUSE(5),DISCARDNEGATIVES(6),EXTENDERRANGE(7),HEAL(8),INVISIBLE(9),POISONCLOUD(10),PURGE(11),THROW(12), ;
 	
 	private int numberRepresentation;
 	/**
---
+++ src/main/java/pl/grm/narutocraftmod/effects/Stun.java
@@ -0,0 +1,5 @@
+package pl.grm.narutocraftmod.effects;
+
+public class Stun extends ItemEffect {
+
+}
