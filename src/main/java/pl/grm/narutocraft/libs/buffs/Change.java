a77b870a0ec172ead9f33530b5f96a5036a1cc01
MrFreesher <Artur@Artur-Komputer>
2014-12-02 21:24:44 +0100

    Add new buffs

---
+++ src/main/java/pl/grm/narutocraft/libs/buffs/Change.java
@@ -0,0 +1,23 @@
+package pl.grm.narutocraft.libs.buffs;
+
+import net.minecraft.entity.EntityLivingBase;
+
+@Deprecated
+// TODO to delete after creating real ones. This is just scheme
+public class Change extends BuffEffect {
+	
+	public Change(int buffID, int duration, int amplifier) {
+		super(buffID, duration, amplifier);
+	}
+	
+	@Override
+	public void stopEffect(EntityLivingBase entityliving) {}
+	
+	@Override
+	protected String jutsuBuffName() {
+		return "test";
+	}
+	
+	@Override
+	public void startEffect(EntityLivingBase paramEntityLivingBase) {}
+}
