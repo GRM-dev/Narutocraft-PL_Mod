45f6db8aaa614cf8de42cf589e1390468969aba8
Freesher <Artur@Artur-PC>
2015-07-14 10:42:26 +0200

    Fix problem with Mobs ID

--- src/main/java/pl/grm/narutocraft/registry/RegMobs.java
+++ src/main/java/pl/grm/narutocraft/registry/RegMobs.java
@@ -8,6 +8,6 @@ public class RegMobs extends NarutoCraft {
 
 	public static void regMobsList() {
 		// EntityRegistry.registerGlobalEntityID(Entity.class, "Entity", 1);
-		EntityRegistry.registerGlobalEntityID(EntityKyuubi.class, "EntityKyuubi", 4809, 2, 2);
+		EntityRegistry.registerGlobalEntityID(EntityKyuubi.class, "EntityKyuubi", EntityRegistry.findGlobalUniqueEntityId(), 2, 2);
 	}
 }
